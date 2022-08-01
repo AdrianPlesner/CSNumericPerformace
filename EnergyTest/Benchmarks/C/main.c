8d47c5f5c47afa3c444b93c1ad9b98434f3bc5d6
Adrian Plesner <adrian.plesner@gmail.com>
2022-08-01 13:29:59 +0200

    Mains to run c and java benchmarks

---
+++ EnergyTest/Benchmarks/C/main.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "scomm.h"
+#include "cmd.h"
+#include "MatMult.h"
+
+void bm(){
+    long k;
+    printf("running...\n");
+    FlatArray();
+    printf("done\n");
+}
+
+
+int main(int argc, char **argv){
+    if(argc<=1){
+        fprintf(stderr,"\nUsage: %s [socket]\n\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    // int s = serveSingleClient(argv[1]);
+    int s = connectTo(argv[1]);
+    if(s<0){
+        printf("Bad socket...");
+        exit(EXIT_FAILURE);
+    }
+    CMD c;
+    writeCmd(s, Ready);
+    do{
+        writeCmd(s, Ready);
+        c=readCmd(s); // expecting go
+        printf("received (expected go): %s", tos(c));
+        bm();
+        writeCmd(s, Done); //
+    } while ((c = readCmd(s)) == Ready );
+    // we should have read done at this point
+    printf("\ndone\n");
+    closeSocket(s);
+}
