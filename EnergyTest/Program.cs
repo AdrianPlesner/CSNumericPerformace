7617452d61683258665a26a9b27e186cfb266c8f
Adrian Plesner <adrian.plesner@gmail.com>
2022-08-12 10:01:59 +0200

    Note loopiterations

--- EnergyTest/Program.cs
+++ EnergyTest/Program.cs
@@ -6,6 +6,21 @@ using CsharpRAPL.CommandLine;
 
 //Console.WriteLine("Hello, World!!");
 
+/* LoopIterations C#:
+ * DFE: 33554432
+ * 
+ * DIL: 1
+ *
+ * MM standard: 256
+ *
+ * MM unsafe: 512
+ *
+ * MM javalike: 512
+ *
+ * PE: 67108864
+ * 
+ */
+
 var options = CsharpRAPLCLI.Parse(args);
 options.PlotResults = true;
 
