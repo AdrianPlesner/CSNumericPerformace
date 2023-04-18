2aaab5003d5f7cd1062a42fc5f00d65bacc11a16
Adrian Plesner <adrian.plesner@gmail.com>
2023-04-18 11:00:55 +0200

    more loopiterations

--- EnergyTest/Benchmarks/LoopIterations.cs
+++ EnergyTest/Benchmarks/LoopIterations.cs
@@ -2,7 +2,7 @@ namespace EnergyTest.Benchmarks;
 
 public static class LoopIterations
 {
-    public const ulong DistFuncEval = 100000000;
+    public const ulong DistFuncEval = 100000000000;
     
-    public const ulong PolyEval = 1000000;
+    public const ulong PolyEval = 10000000000;
 }
